6c1b5474dd6544647423b7759da58c037d761088
sohelsorowar <sohelsorowar@gmail.com>
2019-07-02 17:04:57 +0600

    [registration page]

---
+++ staff_meeting/meeting_app/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.views.generic import View,TemplateView
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponseRedirect, HttpResponse
+#from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+def register(request):
+    registered = False
+
+    if request.method =="POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+
+            profile.save()
+
+            registered = True
+        else:
+                print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+    return render(request,'basic_app/registration.html',
+                               {'user_form':user_form,
+                                'profile_form':profile_form,
+                                 'registered':registered})
